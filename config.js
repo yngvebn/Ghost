2c77a4fe85c9b4fad97c930b2bc209599444013f
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-12-08 20:56:22 +0100

    Wrong port

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: 'process.env.PORT'
+            port: process.env.PORT
         }
     },
 
